d619c1a0224c49d1864cc690edc1492df648b774
BireN <shashwat.biren007@gmail.com>
2024-01-15 00:33:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Form,
+  InputGroup,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function App() {
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+const App = () => {
+  const storedSymbol = localStorage.getItem("symbol") || "EUR/USD";
+  const storedAccountSize = localStorage.getItem("accountSize") || "0";
+  const storedRiskPercentage = localStorage.getItem("riskPercentage") || "0";
+  const storedStopLossPips = localStorage.getItem("stopLossPips") || "0";
+
+  const [symbol, setSymbol] = useState(storedSymbol);
+  const [accountSize, setAccountSize] = useState(storedAccountSize);
+  const [riskPercentage, setRiskPercentage] = useState(storedRiskPercentage);
+  const [stopLossPips, setStopLossPips] = useState(storedStopLossPips);
+  const [lotSize, setLotSize] = useState(0);
+  const [amtRisk, setAmtRisk] = useState(0)
+
+  const pipValues = {
+    "EUR/USD": 10,
+    "USD/JPY": 6.9,
+    "GBP/USD": 10,
+    "USD/CHF": 11.73,
+    "AUD/USD": 10,
+    "USD/CAD": 7.46,
+    "NZD/USD": 10,
+    "EUR/CHF": 11.73,
+    "EUR/JPY": 6.9,
+    NAS100: 10,
+  };
+
+  useEffect(() => {
+    localStorage.setItem("symbol", symbol);
+    localStorage.setItem("accountSize", accountSize);
+    localStorage.setItem("riskPercentage", riskPercentage);
+    localStorage.setItem("stopLossPips", stopLossPips);
+  }, [symbol, accountSize, riskPercentage, stopLossPips]);
+
+  const calculateLotSize = () => {
+    const pipValue = pipValues[symbol];
+    const calculatedLotSize =
+      (parseFloat(accountSize) * (parseFloat(riskPercentage) / 100)) /
+      (parseFloat(stopLossPips) * pipValue);
+    setLotSize(calculatedLotSize.toFixed(2));
+    setAmtRisk(parseFloat(accountSize) * (parseFloat(riskPercentage)) / 100)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="vertical-center">
+        <Container className="glass" fluid>
+          <h2>Lot Size Calculator</h2>
+            <Form.Group controlId="symbol" className="input-field">
+              <Form.Label>Currency Pair</Form.Label>
+              <Form.Control
+                as="select"
+                value={symbol}
+                onChange={(e) => setSymbol(e.target.value)}
+              >
+                {Object.keys(pipValues).map((currency) => (
+                  <option key={currency} value={currency}>
+                    {currency}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="accountSize" className="input-field">
+              <Form.Label>Account Size ($)</Form.Label>
+              <Form.Control
+                type="text"
+                pattern="[0-9]*"
+                value={accountSize}
+                onChange={(e) => setAccountSize(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="riskPercentage" className="input-field">
+              <Form.Label>Risk Percentage</Form.Label>
+              <Form.Control
+                type="text"
+                pattern="[0-9]*"
+                value={riskPercentage}
+                onChange={(e) => setRiskPercentage(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="stopLossPips" className="input-field">
+              <Form.Label>Stop Loss (Pips)</Form.Label>
+              <Form.Control
+                type="text"
+                pattern="[0-9]*"
+                value={stopLossPips}
+                onChange={(e) => setStopLossPips(e.target.value)}
+              />
+            </Form.Group>
+          <div>
+            <div className="button-container">
+              <Button
+                variant="contained"
+                className="bn632-hover bn27"
+                onClick={calculateLotSize}
+              >
+                Calculate Lot Size
+              </Button>
+            </div>
+            {/* <div className="calculated-lot-size">
+              <h3></h3>
+            </div> */}
+            <Row>
+        <Col sm={12} md={6}><div className="box"><h4>Risk Amount <br/>${amtRisk}</h4></div></Col>
+        <br/>
+        <Col sm={12} md={6}><div className="box"><h4>Lot Size:<br/> {lotSize}</h4></div></Col>
+      </Row>
+          </div>
+        </Container>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
